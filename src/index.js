d0daccb557eaa31e8a992a38d0e268bc208a7279
NamedRoger <roge.villa.carrera@hotmail.com>
2021-02-04 19:25:22 -0600

    add functions views main

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const {app, BrowserWindow}  = require('electron');
 
 //ventenas 
 let mainWindow;
+let providersMainWindow;
+let stockMainWindow;
+let customersMainWindow;
 
 app.on('ready',() => {
     mainWindow = new BrowserWindow({
@@ -16,3 +19,15 @@ app.on('ready',() => {
 
     mainWindow.loadURL();
 });
+
+function createProvidesMainWindow() {
+
+}
+
+function createCustomersMainWindow(){
+
+}
+
+function createStockMainWindow(){
+    
+}

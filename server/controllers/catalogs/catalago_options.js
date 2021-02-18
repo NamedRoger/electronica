6a1b8a6da362a740c85929f4ffc2f1e9bc7721be
NamedRoger <roge.villa.carrera@hotmail.com>
2021-02-17 19:38:54 -0600

    add helpers

---
+++ server/controllers/catalogs/catalago_options.js
@@ -0,0 +1,9 @@
+const db = require('../../db/database');
+
+const getOptionsByCodeCatalog = (codeCatalog) => {
+    const query = `SELECT id_option, name, active FROM catalog_options`;
+}
+
+const addOption = () => {
+    
+}
---
+++ server/helpers/index.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @param {string} prefijo 
+ * @param {string} name 
+ * 
+ * esta funcion genera codigos con prefijo 
+ * por si alguna tabla de sql la llega ocupar
+ */
+const generateCode = (prefijo,name) => {
+    let spltiName = name.split(/[\s_-]/);
+    let code = "";
+    spltiName.forEach(s => {
+        if(s.length <5){
+            code += s;
+        }else{
+            code += s.slice(0,5);
+        }
+    });
+    return `${prefijo.toUpperCase()}_${code.toUpperCase()}`;
+}

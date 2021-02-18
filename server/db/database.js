e336f99d32b1eff943d3b5fa8c850375ccb65915
NamedRoger <roge.villa.carrera@hotmail.com>
2021-02-17 19:07:45 -0600

    configure database class

---
+++ server/db/database.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const pathEnv = path.join(process.cwd()+'\\..','.env');
+
+require('dotenv').config({
+    path:pathEnv
+});
+
+const mysql = require('mysql');
+
+module.exports = class Db {
+    constructor(){
+        this.port = process.env.DB_PORT;
+        this.host = process.env.DB_HOST;
+        this.user = process.env.DB_USER;
+        this.password = process.env.DB_PASSWORD;
+        this.db = process.env.DB_DATABASE;
+
+        this.conn = mysql.createConnection({
+            host:this.host,
+            user:this.user,
+            password:this.password,
+            database:this.db,
+            port:this.port,
+        });   
+        this.conn.connect();
+    }
+}

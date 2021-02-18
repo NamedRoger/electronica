1fe274b730c7e34c2a1284548e6a2274849d6799
NamedRoger <roge.villa.carrera@hotmail.com>
2021-02-18 00:23:47 -0600

    create controller notes

---
+++ server/controllers/providers/notes.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const pathEnv = path.join(process.cwd()+'\\..','..','.env');
+require('dotenv').config({
+    path:pathEnv
+});
+const db = require('../../db/database');
+const database = new db();
+
+const getNotesByProvider = (idProvider) => {
+    const query = `SELECT FROM provider_notes WHERE id_provider = ${idProvider}`;
+    return database.query(query);
+}
+
+const addNote = (idProvider,description) =>{
+    const query = `INSERT INTO provider_notes(id_provider,descrption) VALUES(${idProvider},'${description}')`;
+    return database.query(query);
+}
+
+const updateNote = (idNote,description) => {
+    const query = `UPDATE provider_notes SET description = '${description}' WHERE id_note = ${idNote}`;
+    return database.query(query);
+}
+
+const deleteNote = (idNote) => {
+    const query = `DELETE FROM provider_notes WHERE id_note = ${idNote}`;
+    return database.query(query);
+}
+

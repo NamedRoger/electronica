88efaef887e50f85df35abc9a37dec89b066164e
alan-tc <alantamez28@hotmail.com>
2021-02-26 12:53:43 -0600

    tabla clientes

--- front/src/pages/clientes/views/TablaCliente.js
+++ front/src/pages/clientes/views/TablaCliente.js
@@ -7,30 +7,22 @@ export default function TablaCliente() {
         <table className="highlight">
             <thead>
                 <tr>
-                    <th>Nombre</th>
-                    <th>RFC</th>
+                    <th>Clave Registro</th>
+                    <th>Representante</th>
                     <th>Razón Social</th>
-                    <th>Telefonos</th>
-                    <th>Email</th>
-                    <th>Paquete</th>
-                    <th>Direccion Completa</th>
+                    <th>Empresa</th>
                     <th>Acciones</th>
                 </tr>
             </thead>
 
             <tbody>
                 <tr>
-                    <td>Alvin</td>
-                    <td>ALVIN300006KT2</td>
-                    <td>sandbjsa</td>
-                    <td>Telefono: 933848234234
-                    Celular: 827388343432
-            </td>
-                    <td>alvin_7@gmail.com</td>
-                    <td>TM077</td>
-                    <td>Alvin #456 col Noreste Monterrey N.L Mexico C.P 63421</td>
+                    <td>XXX-XXX-XXX</td>
+                    <td>XXX-XXX-XXX	</td>
+                    <td>XXX-XXX-XXX	</td>
+                    <td>XXX-XXX-XXX	</td>
                     <td>
-                        <div className="botones">
+                        <div className="botones container">
                             <a className="btn-floating btn-large waves-effect waves-light light-blue darken-1"
                                 href={pages[0].dropdown[2].link} rel="noreferrer" target="_blank">
                                 <i className="material-icons">edit</i>
@@ -38,12 +30,6 @@ export default function TablaCliente() {
                             <button className="btn-floating btn-large waves-effect waves-light red darken-3">
                                 <i className="material-icons">delete</i>
                             </button>
-                            <a className="waves-effect waves-light grey darken-1 btn-small"
-                                href={pages[0].dropdown[3].link} rel="noreferrer" target="_blank">Bancos</a>
-                            <a className="waves-effect waves-light red lighten-1 btn-small"
-                                href={pages[0].dropdown[4].link} rel="noreferrer" target="_blank">Notas</a>
-                            <a className="waves-effect waves-light blue accent-2 btn-small"
-                                href={pages[0].dropdown[5].link} rel="noreferrer" target="_blank">Observaciones</a>
                         </div>
                     </td>
                 </tr>

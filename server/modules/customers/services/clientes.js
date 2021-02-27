4939da8b8d22cc7a7576d2001ba12e26a95a9fa5
NamedRoger <roge.villa.carrera@hotmail.com>
2021-02-27 11:38:09 -0600

    corregi las consultas de customers

--- server/modules/customers/services/clientes.js
+++ server/modules/customers/services/clientes.js
@@ -17,19 +17,19 @@ const getCliente = (idCliente) => {
     razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business
     FROM customers
     WHERE active = 1`;
-    `AND idCliente = ${idCliente}`;
+    `AND id_customer = ${idCliente}`;
     return database.query(query);
 }
 
-const addCliente = (id_customer, {register_key, id_category, presentative, 
+const addCliente = ({register_key, id_category, presentative, 
     tel_customer, cel_customer, email, note, address, city, state, country, 
     razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business}) => {
         
-    const query = `INSERT INTO customers (id_customer, register_key, id_category, presentative, 
+    const query = `INSERT INTO customers ( register_key, id_category, presentative, 
         tel_customer, cel_customer, email, note, address, city, state, country, 
-        razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business) VALUES ('${id_customer}', '${register_key}',
-        '${id_category}','${presentative}','${tel_customer}','${cel_customer}','${email}','${note}','${address}','${city}','${state}',
-        '${country}','${razon_social}','${rfc}','${bank}','${bank_key}','${price_number}','${business}','${tel_business}',
+        razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business) VALUES ('${register_key}',
+        ${id_category},'${presentative}','${tel_customer}','${cel_customer}','${email}','${note}','${address}','${city}','${state}',
+        '${country}','${razon_social}','${rfc}','${bank}','${bank_key}',${price_number},'${business}','${tel_business}',
         '${cel_business}','${true}') `;
         return database.query(query);
     }
@@ -38,10 +38,10 @@ const updateCliente = (id_customer,{register_key, id_category, presentative,
         tel_customer, cel_customer, email, note, address, city, state, country, 
         razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business}) => {
         
-        const query = `UPDATE providers SET register_key = '${register_key}',id_category = '${id_category}', presentative = '${presentative}',
+        const query = `UPDATE providers SET register_key = '${register_key}', id_category = ${id_category}, presentative = '${presentative}',
         tel_customer = '${tel_customer}', cel_customer = '${cel_customer}', email = '${email}', note = ´${note}', address = '${address}',
         city = '${city}', state = '${state}', country = '${country}', razon_social = '${razon_social}', rfc = '${rfc}', 
-        bank = '${bank}', bank_key = '${bank_key}', price_number = '${price_number}', business = '${business}',
+        bank = '${bank}', bank_key = '${bank_key}', price_number = ${price_number}, business = '${business}',
         tel_business = '${tel_business}', cel_business = '${cel_business}',
         WHERE id_customer = ${id_customer}`;
 

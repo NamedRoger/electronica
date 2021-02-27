de3dd0b405ffd8d99018392566e0d679bd31f61c
A-Damian11 <alandamian11.fr@gmail.com>
2021-02-27 04:34:50 -0600

    customers

--- server/modules/customers/services/clientes.js
+++ server/modules/customers/services/clientes.js
@@ -1,29 +1,64 @@
-const path = require('path');
-const pathEnv = path.join(process.cwd()+'\\..','\\..','..','.env');
-require('dotenv').config({
-    path:pathEnv
-});
+const db = require ('../../../db/database');
 
-console.log((pathEnv));
-// const db = require('../../../db/database');
-// const database = new db();
+const database = new db ();
 
-// const getCustomer  = (idCustomer) => {
-    
-// }
+const getClientes = () => {
+    const query =  `SELECT  id_customer, register_key, id_category, presentative, 
+    tel_customer, cel_customer, email, note, address, city, state, country, 
+    razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business
+    FROM customers
+    WHERE active = 1`;
+    return database.query(query);
+}
 
-// const getCustomers = () => {
+const getCliente = (idCliente) => {
+    const query =  `SELECT  id_customer, register_key, id_category, presentative, 
+    tel_customer, cel_customer, email, note, address, city, state, country, 
+    razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business
+    FROM customers
+    WHERE active = 1`;
+    `AND idCliente = ${idCliente}`;
+    return database.query(query);
+}
 
-// }
+const addCliente = (id_customer, {register_key, id_category, presentative, 
+    tel_customer, cel_customer, email, note, address, city, state, country, 
+    razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business}) => {
+        
+    const query = `INSERT INTO customers (id_customer, register_key, id_category, presentative, 
+        tel_customer, cel_customer, email, note, address, city, state, country, 
+        razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business) VALUES ('${id_customer}', '${register_key}',
+        '${id_category}','${presentative}','${tel_customer}','${cel_customer}','${email}','${note}','${address}','${city}','${state}',
+        '${country}','${razon_social}','${rfc}','${bank}','${bank_key}','${price_number}','${business}','${tel_business}',
+        '${cel_business}','${true}') `;
+        return database.query(query);
+    }
 
-// const addCustomer = () => {
+const updateCliente = (id_customer,{register_key, id_category, presentative, 
+        tel_customer, cel_customer, email, note, address, city, state, country, 
+        razon_social, rfc, bank, bank_key, price_number, business, tel_business, cel_business}) => {
+        
+        const query = `UPDATE providers SET register_key = '${register_key}',id_category = '${id_category}', presentative = '${presentative}',
+        tel_customer = '${tel_customer}', cel_customer = '${cel_customer}', email = '${email}', note = ´${note}', address = '${address}',
+        city = '${city}', state = '${state}', country = '${country}', razon_social = '${razon_social}', rfc = '${rfc}', 
+        bank = '${bank}', bank_key = '${bank_key}', price_number = '${price_number}', business = '${business}',
+        tel_business = '${tel_business}', cel_business = '${cel_business}',
+        WHERE id_customer = ${id_customer}`;
 
-// }
+    return database.query(query);
+        
+}
 
-// const updateCustomer = () => {
+const desactiveCustomer = (id_customer) => {
+    const query = `UPDATE providers SET active = ${false} WHERE id_customer = ${id_customer}`;
+    return database.query(query);
+}
 
-// }
+module.exports = {
+    getCliente, 
+    getClientes,
+    addCliente,
+    updateCliente,
+    desactiveCustomer
+}
 
-// const desactiveCustomer = () => {
-
-// }

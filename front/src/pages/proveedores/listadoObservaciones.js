9f1f3b6f16685a492736665241bf38a5f4d3fdfd
NamedRoger <roge.villa.carrera@hotmail.com>
2021-03-01 01:06:54 -0600

    observaciones

--- front/src/pages/proveedores/listadoObservaciones.js
+++ front/src/pages/proveedores/listadoObservaciones.js
@@ -1,58 +1,152 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
+import Loader from '../../components/Loader';
+import { addObservation, deleteObservation, getObservations, updateObservation } from '../../services/Providers/observationsService';
 
-export default function ListadoObservaciones() {
+export default function ListadoObservaciones({ match }) {
+    const [observations, setObservations] = useState([]);
+    const [description, setDescription] = useState('');
+    const [load, setLoad] = useState(false);
+    const [edit, setEdit] = useState({
+        edit: false,
+        id: null
+    });
 
-    console.log("hola");
-        useEffect(()=>{
-            document.title= 'Listado Observaciones';
-        }, []);
+    useEffect(() => {
+        document.title = 'Listado Observaciones';
+
+    }, []);
+
+    useEffect(() => {
+        getObservations(match.params.id).then(res => {
+            setObservations(res);
+            setLoad(true);
+        });
+    }, [match.params.id, load]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let res;
+        try {
+            if (!edit.edit) {
+                console.log();
+                console.log(description);
+                res = await addObservation(match.params.id, { description: description });
+            } else {
+                res = await updateObservation(match.params.id, edit.id, { description });
+            }
+
+            console.log(res);
+
+            setDescription('');
+            setEdit({
+                edit: false,
+                id: null
+            });
+            setLoad(false);
+        } catch (e) {
+            alert(e);
+        }
+    }
+
+    const handleDelete = async (idObservation) => {
+        try {
+            const res = await deleteObservation(match.params.id, idObservation);
+            if (res.status !== 204) throw "Ocurrió un error";
+            setLoad(false);
+        } catch (e) {
+            alert('Ocurrió un error');
+        }
+
+        setDescription('');
+        setEdit({
+            edit: false,
+            id: null
+        });
+    }
+
+    const onEdit = (id, { description }) => {
+        setEdit({
+            edit: true,
+            id
+        });
+
+        setDescription(description);
+    }
+
+
+    const handleClose = () => {
+        setEdit({
+            edit: false,
+            id: null
+        });
+        setDescription('');
+    }
+
+    if (!load) {
+        return <Loader />
+    }
 
     return (
         <div>
             <section className="container">
-        <div className="row">
-            <form className="col s12">
                 <div className="row">
-                    <div className="input-field col s12">
-                        <textarea id="textarea1" className="materialize-textarea" ></textarea>
-                        <label htmlfor="textarea1">Descripción</label>
-                    </div>
+                    <form className="col s12" onSubmit={handleSubmit}>
+                        <div className="row">
+                            {edit.edit && <div className="col s6">
+                                <strong>{`ID del banco: ${edit.id}`}</strong>
+                            </div>}
+                            <div className="input-field col s12">
+                                <input id="first_name" type="text" placeholder="description"
+                                    name="description" value={description}
+                                    onChange={(e) => { setDescription(e.target.value) }} className="validate" required />
+                            </div>
+
+                        </div>
+                        <div className="buttons">
+                            <button className="waves-effect waves-light btn-small">Guardar</button>
+                        </div>
+                    </form>
+                    {edit.edit && <button className="waves-effect waves-light btn-small red"
+                        onClick={handleClose}>Cancelar</button>}
+                    {observations.length === 0 ? <div style={{
+                        paddingTop: '3em',
+                        textAlign: 'center'
+                    }}><h2>No hay observaciones</h2></div> : <table className="col s12">
+                            <thead>
+                                <tr>
+                                    <th>Nombre</th>
+                                    <th>Cuenta de banco</th>
+                                    <th>Llave Banco</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {observations.map(obsertvation => {
+                                    return (
+                                        <tr key={obsertvation.id_observation}>
+                                            <td>{obsertvation.description}</td>
+                                            <td>
+                                                <div className="botones">
+                                                    <button className="btn-floating btn-large waves-effect waves-light light-blue darken-1"
+                                                        onClick={() => onEdit(obsertvation.id_observation, {
+                                                            description: obsertvation.description
+                                                        })}>
+                                                        <i className="material-icons">edit</i>
+                                                    </button>
+                                                    <button className="btn-floating btn-large waves-effect waves-light red darken-3"
+                                                        onClick={() => handleDelete(obsertvation.id_observation)}>
+                                                        <i className="material-icons">delete</i>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>}
                 </div>
-            </form>
-            <div className="buttons">
-                <button className="waves-effect waves-light btn-small green">Guardar</button>
-                <button className="waves-effect waves-light btn-small blue">Editar</button>
-                <button className="waves-effect waves-light btn-small red">Eliminar</button>
-            </div>
-            <table className="highlight">
-                <thead>
-                    <tr>
-                        <th>Descripción Nota: </th>
-                        <th>Fecha: </th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    <tr>
-                        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in lacus sed est ultricies
-                            ullamcorper. Integer eleifend sapien in massa pulvinar sollicitudin. </td>
-                        <td>2/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in lacus sed est ultricies
-                            ullamcorper. Integer eleifend sapien in massa pulvinar sollicitudin. </td>
-                        <td>2/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in lacus sed est ultricies
-                            ullamcorper. Integer eleifend sapien in massa pulvinar sollicitudin. </td>
-                        <td>2/02/2021</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-    </section>
+            </section>
         </div>
     )
 }
--- front/src/pages/proveedores/views/TablaProveedores.js
+++ front/src/pages/proveedores/views/TablaProveedores.js
@@ -47,7 +47,9 @@ export default function TablaProveedores() {
     window.open(`${pages[0].dropdown[3].link}/${id}`, null, "width=800,height=600,left=300");
   }
 
-  const handleObservaciones = (id) => window.open(`${pages[0].dropdown[5].link}/${id}`, null, "width=800,height=600,left=300");
+  const handleObservaciones = (id) => {
+    window.open(`${pages[0].dropdown[5].link}/${id}`, null, "width=800,height=600,left=300");
+  }
 
   const onDelete = (id) =>{
     const instance = M.Modal.getInstance(modal.current);
@@ -157,7 +159,7 @@ export default function TablaProveedores() {
             href={pages[0].dropdown[4].link} rel="noreferrer" target="_blank">Notas</a>
 
             <button className="waves-effect waves-light blue accent-2 btn-small" 
-            onClick={() => handleBancos(prov.id_provider)}>Observaciones</button>
+            onClick={() => handleObservaciones(prov.id_provider)}>Observaciones</button>
             </div>
             </td>
           </tr>

55b1dbda62f9a56f9f5d02a31d16390bec4821bc
alan-tc <alantamez28@hotmail.com>
2021-03-02 15:05:56 -0600

    edit titulo y boton

--- front/src/pages/almacen/Almacen.js
+++ front/src/pages/almacen/Almacen.js
@@ -1,22 +1,22 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { pages } from '../../helpers/pages';
 import TablaAlmacen from './views/TablaAlmacen';
 import './index.css';
 
 export default function Proveedores() {
-    useEffect(()=>{
-      document.title= 'Almacén';
+  useEffect(() => {
+    document.title = 'Almacén';
   }, []);
-  const handleOpen = ()=>{
+  const handleOpen = () => {
     window.open(pages[2].dropdown[0].link, null, "width=800,height=600,left=300");
   }
-    return (
-        <div className="">
-            <h1 className="center-align">Almacén</h1>
-            <button className="waves-effect waves-light green btn-large" 
-            style={{display: 'block', margin: '2em 32vw'}} onClick={handleOpen}>
-              <i className="material-icons left">add</i>Agregar entrada</button>
-              <TablaAlmacen />
-        </div>
-    )
+  return (
+    <div className="">
+      <h4>Almacén</h4>
+      <button className="waves-effect waves-light green btn btn-small"
+        onClick={handleOpen}>
+        <i className="material-icons left">add</i>Agregar entrada</button>
+      <TablaAlmacen />
+    </div>
+  )
 }

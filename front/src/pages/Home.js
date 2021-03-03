76ffd63d68912a1552e93df5370aa74a821f6ca5
NamedRoger <roge.villa.carrera@hotmail.com>
2021-03-03 13:57:23 -0600

    resize icom

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -11,11 +11,11 @@ export default function Home() {
       <div className="row">
         {pages.map((target, index) => {
           return (
-            <div className="col s4" key={index}>
+            <div className="col s4" key={index} >
               <Link to={target.link}>
                 <div className="card card-panel hoverable">
                   <div className="card-image">
-                    <img src={target.img} alt={target.name} />
+                    <img src={target.img} alt={target.name} style={{width:'10rem'}}/>
                   </div>
                   <div className="card-action">
                     <h5 className="text center-align">{target.name}</h5>

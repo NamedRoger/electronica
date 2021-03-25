94ad22351f197a1f8c41645c8c3012a7df647300
alan-tc <alantamez28@hotmail.com>
2021-03-25 16:24:51 -0600

    cambios de letra y tamaño de botones clientes

--- front/src/pages/clientes/views/TablaCliente.js
+++ front/src/pages/clientes/views/TablaCliente.js
@@ -5,24 +5,27 @@ import { getCustomers, desactiveCustomer } from '../../../services/customers/cus
 import M from 'materialize-css';
 import { Link } from 'react-router-dom';
 import Loader from '../../../components/Loader';
-
-const filterProviders = (filterText,...filters) => {
+const fontSizeTitle = { fontSize: '15px' };
+const fontSizeNormal = { fontSize: '12px' };
+const sizeButton = { width: '40px', height: '40px' };
+const sizeIcon = { lineHeight: '35px' };
+const filterProviders = (filterText, ...filters) => {
     filterText = filterText.trim();
     const worsdFilterText = filterText.split(" ");
     const proveedores = [];
     let provedoresFilter = proveedores;
-  
-    if(filterText === ""){
-    }else{
+
+    if (filterText === "") {
+    } else {
         provedoresFilter = provedoresFilter.filter((provider) => {
             //apodo, repre, repre tel, reprkwe cel, repre email,repre ciudad, razón s, rfc, ciudad, tel, email
-            for(let idxFilter in filters){
+            for (let idxFilter in filters) {
                 const filter = filters[idxFilter];
 
-                for(let idxWord in worsdFilterText){
+                for (let idxWord in worsdFilterText) {
                     const word = worsdFilterText[idxWord];
 
-                    if(includes(provider[filter],word)) return true;
+                    if (includes(provider[filter], word)) return true;
                 }
             }
             return false;
@@ -31,11 +34,11 @@ const filterProviders = (filterText,...filters) => {
 }
 
 
-const includes = (a,b) =>{ 
-    if(a === undefined || a === null){
+const includes = (a, b) => {
+    if (a === undefined || a === null) {
         return false;
-    }else{
-       return a.toUpperCase().includes(b.toUpperCase());
+    } else {
+        return a.toUpperCase().includes(b.toUpperCase());
     }
 };
 export default function TablaCliente() {
@@ -86,60 +89,59 @@ export default function TablaCliente() {
         setChange(false);
     }
 
-    if(!load){
-        return <Loader />   
-     }
+    if (!load) {
+        return <Loader />
+    }
     return (
         <div>
-            
-            {clientes.length===0 || clientes.error ? 
-            <h2
-            style={{
-              textAlign: 'center',
-              width: '100%',
-              height: '100px',
-              left: '20%',
-              top: '50px'
-            }}
-          >{clientes.error ? clientes.error : 'No hay elementos en clientes'}</h2>
-            :
-            <table className="highlight">
-                <thead>
-                    <tr>
-                        <th>Clave Registro</th>
-                        <th>Representante</th>
-                        <th>Razón Social</th>
-                        <th>Empresa</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {clientes.map((cliente) => {
-                        return (
-                            <tr key={cliente.id_customer} >
-                                <td>{cliente.register_key}</td>
-                                <td>{cliente.representative}</td>
-                                <td>{cliente.razon_social}    </td>
-                                <td>{cliente.business}</td>
-                                <td>
-                                    <div className="botones container">
-                                        <Link to={`${pages[1].dropdown[2].link}/${cliente.id_customer}`}>
-                                        <button className="btn-floating btn-large waves-effect waves-light light-blue darken-1"
-                                         >
-                                            <i className="material-icons">edit</i>
-                                        </button>
-                                        </Link>
-                                        <button className="btn-floating btn-large waves-effect waves-light red darken-3"
-                                            onClick={() => { handleDelete(cliente.id_customer) }}>
-                                            <i className="material-icons">delete</i>
-                                        </button>
-                                    </div>
-                                </td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>}
+
+            {clientes.length === 0 || clientes.error ?
+                <h2
+                    style={{
+                        textAlign: 'center',
+                        width: '100%',
+                        height: '100px',
+                        left: '20%',
+                        top: '50px'
+                    }}
+                >{clientes.error ? clientes.error : 'No hay elementos en clientes'}</h2>
+                :
+                <table className="highlight">
+                    <thead>
+                        <tr>
+                            <th style={fontSizeTitle}>Clave Registro</th>
+                            <th style={fontSizeTitle}>Representante</th>
+                            <th style={fontSizeTitle}>Razón Social</th>
+                            <th style={fontSizeTitle}>Empresa</th>
+                            <th style={fontSizeTitle}>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {clientes.map((cliente) => {
+                            return (
+                                <tr key={cliente.id_customer} >
+                                    <td style={fontSizeNormal}>{cliente.register_key}</td>
+                                    <td style={fontSizeNormal}>{cliente.representative}</td>
+                                    <td style={fontSizeNormal}>{cliente.razon_social}    </td>
+                                    <td style={fontSizeNormal}>{cliente.business}</td>
+                                    <td>
+                                        <div className="botones container">
+                                            <Link to={`${pages[1].dropdown[2].link}/${cliente.id_customer}`}>
+                                                <button className="btn-floating btn-large waves-effect waves-light light-blue darken-1" style={sizeButton}>
+                                                    <i className="material-icons" style={sizeIcon}>edit</i>
+                                                </button>
+                                            </Link>
+                                            <button className="btn-floating btn-large waves-effect waves-light red darken-3"
+                                                onClick={() => { handleDelete(cliente.id_customer) }} style={sizeButton} >
+                                                <i className="material-icons" style={sizeIcon}>delete</i>
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>}
         </div>
     )
 }

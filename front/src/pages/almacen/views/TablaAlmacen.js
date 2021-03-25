485c67f1a8772bc014a420921da6ce925684aadb
alan-tc <alantamez28@hotmail.com>
2021-03-25 16:56:14 -0600

    cambios de letra y tamaño de botones almacen

--- front/src/pages/almacen/views/TablaAlmacen.js
+++ front/src/pages/almacen/views/TablaAlmacen.js
@@ -9,6 +9,10 @@ export default function TablaProveedores() {
   const [almacen, setAlmacen] = useState([]);
   const [load, setLoad] = useState(false);
   const [catalog, setCatalog] = useState([]);
+  const fontSizeTitle = { fontSize: '15px' };
+  const fontSizeNormal = { fontSize: '12px' };
+  const sizeButton = { width: '40px', height: '40px' };
+  const sizeIcon = { lineHeight: '35px' };
 
   useEffect(() => {
     getStocks()
@@ -51,15 +55,15 @@ export default function TablaProveedores() {
           top: '50px'
         }}
       >{almacen.error ? almacen.error : 'No hay elementos en almacen'}</h2>
-      :
+        :
         <table className="highlight">
           <thead>
             <tr>
-              <th>Producto</th>
-              <th>Descripción</th>
-              <th>Categoría</th>
-              <th>Existencias</th>
-              <th>Acciones</th>
+              <th style={fontSizeTitle}>Producto</th>
+              <th style={fontSizeTitle}>Descripción</th>
+              <th style={fontSizeTitle}>Categoría</th>
+              <th style={fontSizeTitle}>Existencias</th>
+              <th style={fontSizeTitle}>Acciones</th>
             </tr>
           </thead>
 
@@ -68,27 +72,26 @@ export default function TablaProveedores() {
               let cat = catalog.find(e => stock.id_category === e.id_option)
               return (
                 <tr key={stock.id_product}>
-                  
-                  <td><img src={`img/${stock.foto}`} alt="" style={{width:'4rem'}}></img></td>
-                  <td>{stock.description}</td>
-                  <td>{cat.name}</td>
-                  <td>{stock.stock}</td>
+
+                  <td><img src={`img/${stock.foto}`} alt="" style={{ width: '4rem' }}></img></td>
+                  <td style={fontSizeNormal}>{stock.description}</td>
+                  <td style={fontSizeNormal}>{cat.name}</td>
+                  <td style={fontSizeNormal}>{stock.stock}</td>
                   <td>
                     <div className="botones">
                       <Link to={`${pages[2].dropdown[1].link}/${stock.id_product}`}>
-                      <button className="btn-floating btn-large waves-effect waves-light light-blue darken-1"
-                        >
-                        <i className="material-icons" id="editar">edit</i>
-                      </button>
+                        <button className="btn-floating btn-large waves-effect waves-light light-blue darken-1" style={sizeButton}>
+                          <i className="material-icons" id="editar" style={sizeIcon}>edit</i>
+                        </button>
                       </Link>
-                      <button className="btn-floating btn-large waves-effect waves-light red darken-3">
-                        <i className="material-icons" onClick={() => handleDelete(stock.id_product)}>delete</i>
+                      <button className="btn-floating btn-large waves-effect waves-light red darken-3" style={sizeButton}>
+                        <i className="material-icons" style={sizeIcon} onClick={() => handleDelete(stock.id_product)}>delete</i>
                       </button>
                       <Link to={`${pages[2].dropdown[2].link}/${stock.id_product}`}>
-                      <button className="waves-effect waves-light grey darken-1 btn-small"
-                         id="proveedor">
+                        <button className="waves-effect waves-light grey darken-1 btn-small"
+                          id="proveedor" style={{width: '126px', fontSize: '10px'}}>
                           Ver proveedores</button>
-                          </Link>
+                      </Link>
                     </div>
                   </td>
                 </tr>
--- front/src/pages/almacen/views/TablaProductProvedor.js
+++ front/src/pages/almacen/views/TablaProductProvedor.js
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import {addProvider,getProviders,deleteProvider,updateProvider} from '../../../services/Stock/prodcutProviders';
+import { addProvider, getProviders, deleteProvider, updateProvider } from '../../../services/Stock/prodcutProviders';
 import * as providersService from '../../../services/Providers/getProviders';
 import { useHistory } from 'react-router-dom';
 
 export default function TablaProveedoresProducto({ match }) {
     const [proveedores, setProveedores] = useState([]);
     const [proveedoresSelect, setProveedoresSelect] = useState([]);
-    const [idProvider,setIdProvider] = useState(0);
-    const [sales_unit,setSalesUnit] = useState('');
-    const [idProductProvider,setIdProductProvider] = useState(0);
+    const [idProvider, setIdProvider] = useState(0);
+    const [sales_unit, setSalesUnit] = useState('');
+    const [idProductProvider, setIdProductProvider] = useState(0);
     const idProduct = match.params.id;
     let history = useHistory();
+    const fontSizeTitle = { fontSize: '15px' };
+    const fontSizeNormal = { fontSize: '12px' };
+    const sizeButton = { width: '40px', height: '40px' };
+    const sizeIcon = { lineHeight: '35px' };
 
     useEffect(() => {
         document.title = 'Proveedores Producto';
@@ -22,11 +26,11 @@ export default function TablaProveedoresProducto({ match }) {
             setProveedores(providers);
         })();
 
-        (async() => {
+        (async () => {
             const providers = await providersService.getProviders();
             setProveedoresSelect(providers);
         })();
-    },[match.params.id]);
+    }, [match.params.id]);
 
     const resetForm = () => {
         setIdProvider(0);
@@ -34,7 +38,7 @@ export default function TablaProveedoresProducto({ match }) {
         setSalesUnit('');
     }
 
-    const loadForm = (idProductProvider,idProvider,sales_unit) => {
+    const loadForm = (idProductProvider, idProvider, sales_unit) => {
         setIdProductProvider(idProductProvider);
         setIdProvider(idProvider);
         setSalesUnit(sales_unit);
@@ -42,72 +46,72 @@ export default function TablaProveedoresProducto({ match }) {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
-            if(idProductProvider === 0){
-                await addProvider({idProduct:idProduct, idProvider:idProvider,sales_unit:sales_unit});
-            }else{
-                await updateProvider(idProduct,idProductProvider,sales_unit);
+        try {
+            if (idProductProvider === 0) {
+                await addProvider({ idProduct: idProduct, idProvider: idProvider, sales_unit: sales_unit });
+            } else {
+                await updateProvider(idProduct, idProductProvider, sales_unit);
             }
 
             const providers = await getProviders(match.params.id);
             setProveedores(providers);
 
             resetForm();
-        }catch(e){
+        } catch (e) {
             alert("Ocurrió un error");
         }
     }
 
     const handleEdit = (idProductProvider) => {
         const provider = proveedores.find(p => p.id_product_providers === idProductProvider);
-        loadForm(provider.id_product_providers,provider.id_provider,provider.sales_unit);
+        loadForm(provider.id_product_providers, provider.id_provider, provider.sales_unit);
 
     }
 
     const handleDelete = async (idProductProvider) => {
-        await deleteProvider(idProduct,idProductProvider);
+        await deleteProvider(idProduct, idProductProvider);
         resetForm();
         const providers = await getProviders(match.params.id);
         setProveedores(providers);
     }
     return (
         <div className="">
-            <button type="button" 
-              className="btn-floating btn-large waves-effect waves-light red left" 
-              style={{ display: 'block', marginBottom: '30px' }}
-              onClick={() => {history.push('/almacen')}}>
-                <i className="material-icons">arrow_back</i>
+            <button type="button"
+                className="btn-floating btn-large waves-effect waves-light red left"
+                style={{ display: 'block', marginBottom: '30px', width: '40px', height: '40px' }}
+                onClick={() => { history.push('/almacen') }}>
+                <i className="material-icons" style={sizeIcon}>arrow_back</i>
             </button>
             <div className="container">
-            <h4 className="center-align">Proveedores</h4>
+                <h4 className="center-align">Proveedores</h4>
                 <form onSubmit={handleSubmit}>
                     <div className="row">
-                    <div className="input-field col s6">
-                        <select className="browser-default" defaultValue={'0'} name="id_provider" value={idProvider} onChange={(e) => {setIdProvider(e.target.value)}}>
-                            <option value="0"  > -- Proveedor-- </option>
-                            {proveedoresSelect.map(p => (
-                                <option key={p.id_provider} value={p.id_provider}>{p.nick_name} </option>
-                            ))}
-                        </select>
-                    </div>
-                    <div className="input-field col s6">
-                        <input id="first_name" placeholder="Unidad de Compra" 
-                        type="text" 
-                        value={sales_unit} 
-                        onChange={(e) => {setSalesUnit(e.target.value)}}
-                        className="validate" name="sales_unit"/>
-                        <label htmlFor="first_name">Unidad de Compra</label>
-                    </div>
-                    <div className="col">
-                        <button type="submit" className="waves-effect waves-light green btn btn-small">
-                           Guardar
+                        <div className="input-field col s6">
+                            <select className="browser-default" defaultValue={'0'} name="id_provider" value={idProvider} onChange={(e) => { setIdProvider(e.target.value) }}>
+                                <option value="0"  > -- Proveedor-- </option>
+                                {proveedoresSelect.map(p => (
+                                    <option key={p.id_provider} value={p.id_provider}>{p.nick_name} </option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="input-field col s6">
+                            <input id="first_name" placeholder="Unidad de Compra"
+                                type="text"
+                                value={sales_unit}
+                                onChange={(e) => { setSalesUnit(e.target.value) }}
+                                className="validate" name="sales_unit" />
+                            <label htmlFor="first_name">Unidad de Compra</label>
+                        </div>
+                        <div className="col">
+                            <button type="submit" className="waves-effect waves-light green btn btn-small" style={{width: '75px', fontSize: '10px'}}>
+                                Guardar
                         </button>
 
-                      
-                    </div>
-                    <div>
-                        <button className="waves-effect waves-light btn-small red " onClick={() => {resetForm()}}>Cancelar</button>
-                    </div>
+
+                        </div>
+                        <div>
+                            <button className="waves-effect waves-light btn-small red " style={{width: '78px', fontSize: '10px'}} onClick={() => { resetForm() }}>Cancelar</button>
+                        </div>
                     </div>
                 </form>
             </div>
@@ -115,25 +119,25 @@ export default function TablaProveedoresProducto({ match }) {
             <table className="highlight">
                 <thead>
                     <tr>
-                        <th>Nombre</th>
-                        <th>Razon Social</th>
-                        <th>Unidad de ventas</th>
-                        <th>Acciones</th>
+                        <th style={fontSizeTitle}>Nombre</th>
+                        <th style={fontSizeTitle}>Razon Social</th>
+                        <th style={fontSizeTitle}>Unidad de ventas</th>
+                        <th style={fontSizeTitle}>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
                     {proveedores.map(p => (
                         <tr key={p.id_product_providers}>
-                            <td>{p.nick_name}</td>
-                            <td>{p.razon_social}</td>
-                            <td>{p.sales_unit}</td>
+                            <td style={fontSizeNormal}>{p.nick_name}</td>
+                            <td style={fontSizeNormal}>{p.razon_social}</td>
+                            <td style={fontSizeNormal}>{p.sales_unit}</td>
                             <td>
-                                <button className="btn-floating btn-large waves-effect waves-light light-blue darken-1"
+                                <button className="btn-floating btn-large waves-effect waves-light light-blue darken-1" style={sizeButton}
                                     onClick={() => handleEdit(p.id_product_providers)}>
-                                    <i className="material-icons" id="editar">edit</i>
+                                    <i className="material-icons" id="editar" style={sizeIcon}>edit</i>
                                 </button>
-                                <button className="btn-floating btn-large waves-effect waves-light red darken-3">
-                                    <i className="material-icons" onClick={() => handleDelete(p.id_product_providers)}>delete</i>
+                                <button className="btn-floating btn-large waves-effect waves-light red darken-3" style={sizeButton}>
+                                    <i className="material-icons" style={sizeIcon} onClick={() => handleDelete(p.id_product_providers)}>delete</i>
                                 </button>
                             </td>
                         </tr>
